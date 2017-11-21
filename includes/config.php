37afd03fc11aa58f0ee3b7bbcc91c4a10ad95f74
Bunias <PJOpozda@gmail.com>
2017-11-21 02:19:13 +0000

    Create config file for db credentals

---
+++ includes/config.php
@@ -0,0 +1,15 @@
+<?php
+
+// Define Database constants
+
+// Define host
+defined("DB_SERVER") ? null : define("DB_SERVER", "host");
+
+// Define database user
+defined("DB_USER")   ? null : define("DB_USER", "username");
+
+// Define database password
+defined("DB_PASS")   ? null : define("DB_PASS", "password");
+
+//Define database name
+defined("DB_NAME")   ? null : define("DB_NAME", "database name");
